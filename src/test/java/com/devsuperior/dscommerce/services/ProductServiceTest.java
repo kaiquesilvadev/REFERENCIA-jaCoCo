44c5f4185482c087dbed4ee038398d8ef2c6da8c
kaiquesilvadev <j.kaique1570@gmail.com>
2024-02-15 20:44:42 -0300

    teste unitario, findAll de product

--- src/test/java/com/devsuperior/dscommerce/services/ProductServiceTest.java
+++ src/test/java/com/devsuperior/dscommerce/services/ProductServiceTest.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dscommerce.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,10 +15,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.factory.CriaProduct;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
@@ -26,40 +33,55 @@ public class ProductServiceTest {
 
 	@InjectMocks
 	private ProductService service;
-	
+
 	@Mock
 	private ProductRepository repository;
-	private Long existente , inexistente;
+	private Long existente, inexistente;
 	private Product product;
-	
+	private PageImpl<Product> page;
+
 	@BeforeEach
-	public void setUp() throws Exception{
-		product = CriaProduct.productComId();
-		ProductDTO dto = new ProductDTO(product);
-		
+	public void setUp() throws Exception {
 		existente = 1L;
 		inexistente = 1000L;
-		
+		product = CriaProduct.productComId();
+		ProductDTO dto = new ProductDTO(product);
+		page = new PageImpl<>(List.of(product));
+
 		Mockito.when(repository.findById(existente)).thenReturn(Optional.of(product));
-	    Mockito.when(repository.findById(inexistente)).thenReturn(Optional.empty());
+		Mockito.when(repository.findById(inexistente)).thenReturn(Optional.empty());
+		Mockito.when(repository.searchByName(any(), (Pageable) any())).thenReturn(page);
 	}
-	
+
 	@Test
 	@DisplayName("findById Deve Retornar Um Dto Quando Id For Existente")
 	public void findByIdDeveRetornarUmDtoQuandoIdForExistente() {
 		ProductDTO productDTO = service.findById(existente);
-		
+
 		assertEquals(productDTO.getId(), 1L);
-		assertEquals(productDTO.getName() , product.getName());
+		assertEquals(productDTO.getName(), product.getName());
 	}
-	
+
 	@Test
 	@DisplayName("findById Deve Retornar Uma Exception quando Id For Inexistente")
-	public void findByIdDeveRetornarUmaExceptionquandoIdForInexistente() {
-		
+	public void findByIdDeveRetornarUmaExceptionQuandoIdForInexistente() {
+
 		assertThrows(ResourceNotFoundException.class, () -> {
 			@SuppressWarnings("unused")
 			ProductDTO productDTO = service.findById(inexistente);
 		});
 	}
+
+	@Test
+	@DisplayName("findAll Deve Retornar Uma Pagina quando Passar Um Nome")
+	public void findAllDeveRetornarUmaPaginaQuandoPassarUmNome() {
+
+		PageRequest pageRequest = PageRequest.of(0, 12);
+		Page<ProductMinDTO> pageProduct = service.findAll("The Lord of the Rings", pageRequest);
+		
+		assertNotNull(pageProduct);
+		assertEquals(pageProduct.getSize(), 1L);
+		assertEquals(pageProduct.iterator().next().getName(), "The Lord of the Rings");
+	}
+
 }
